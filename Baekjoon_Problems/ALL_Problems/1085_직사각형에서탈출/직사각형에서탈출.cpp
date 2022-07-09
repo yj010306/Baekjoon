39b5ec15cd8ce10164154aa7cf5d8ce989d34c3f
yj010306 <97457418+yj010306@users.noreply.github.com>
2022-07-10 02:45:04 +0900

    Update 직사각형에서탈출.cpp

--- Baekjoon_Problems/ALL_Problems/1085_직사각형에서탈출/직사각형에서탈출.cpp
+++ Baekjoon_Problems/ALL_Problems/1085_직사각형에서탈출/직사각형에서탈출.cpp
@@ -11,12 +11,12 @@ int main() {
     int x,y,w,h;
     int Min = 1000;     // 처음으로 비교하는 숫자는 무조건 최솟값이 되어야 하기 때문에 Min의 초기값을 1000으로 설정
 
-    cin >> x >> y >> w >> h;
+    cin >> x >> y >> w >> h;      // 입력
 
-    Min = compare(Min,x);
+    Min = compare(Min,x);       
     Min = compare(Min,y);
-    Min = compare(Min,w-x);
-    Min = compare(Min,h-y);
+    Min = compare(Min,w-x);      // 코드가 위에서 차례로 실행됨
+    Min = compare(Min,h-y);      // 결국 x, y, w-x, h-y를 전부 비교하는 셈.( 비교해야 하는 숫자가 많아지면 어떻게 해야할까...? )
 
     cout << Min << endl;
 
